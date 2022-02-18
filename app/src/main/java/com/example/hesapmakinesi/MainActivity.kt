f95cfa17d0fb731929854fa42b1f17970b67ce3a
MehmetGenc97 <35899288+MehmetGenc97@users.noreply.github.com>
2022-02-18 10:53:37 +0300

    Hesap Makinesi

--- app/src/main/java/com/example/hesapmakinesi/MainActivity.kt
+++ app/src/main/java/com/example/hesapmakinesi/MainActivity.kt
@@ -7,12 +7,78 @@ import android.widget.Button
 import com.example.hesapmakinesi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var tasarim: ActivityMainBinding
     private var metin = "0"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         tasarim = ActivityMainBinding.inflate(layoutInflater)
         setContentView(tasarim.root)
+
+        val buttons = butonListe()
+
+        for (button in buttons) {
+            button.setOnClickListener {
+                sayıGir(button);
+            }
+        }
+
+        tasarim.buttonEsittir.setOnClickListener {
+            topla()
+        }
+
+        tasarim.buttonTemizle.setOnClickListener {
+            temizle()
+        }
+
+        tasarim.buttonGerial.setOnClickListener {
+            geriAl()
+        }
+    }
+
+    private fun sayıGir(button: Button) {
+        if (metin == "0") {
+            metin = button.text.toString()
+        } else if (button.text == "+") {
+            if (!metin.contains("+") && metin != "0")
+                metin += button.text.toString()
+        } else {
+            metin += button.text.toString()
+        }
+        tasarim.sonucEkrani.text = metin
+    }
+
+    private fun geriAl() {
+        tasarim.sonucEkrani.text = metin
+        if (tasarim.sonucEkrani.text.isEmpty())
+            tasarim.sonucEkrani.text = "0"
+        else
+            metin = metin.substring(0, metin.length-1)
+    }
+
+    private fun temizle() {
+        metin = "0"
+        tasarim.sonucEkrani.text = "0"
+    }
+
+    private fun topla() {
+        val c = metin.substring(metin.length-1)
+
+        if((c != "+") && (metin.contains("+"))) {
+            val indeksArtı = metin.indexOf("+")
+            if(indeksArtı == metin.length)
+                tasarim.sonucEkrani.text = metin
+            val kisim1 = metin.subSequence(0, indeksArtı).toString()
+            val kisim2 = metin.subSequence(indeksArtı+1, metin.length).toString()
+            metin = (kisim1.toInt() + kisim2.toInt()).toString()
+
+            tasarim.sonucEkrani.text = metin
+        }
+    }
+
+    private fun butonListe() : ArrayList<Button> {
         val buttons = ArrayList<Button>()
 
         buttons.add(tasarim.buton0)
@@ -27,53 +93,6 @@ class MainActivity : AppCompatActivity() {
         buttons.add(tasarim.buton9)
         buttons.add(tasarim.buttonTopla)
 
-        for (button in buttons) {
-            button.setOnClickListener {
-
-                if (metin == "0") {
-                    metin = ""
-                    metin = button.text.toString()
-                } else if (button.text == "+") {
-                    if (!metin.contains("+") && metin != "0")
-                        metin += button.text.toString()
-                } else {
-                    metin += button.text.toString()
-                }
-                tasarim.sonucEkrani.text = metin
-            }
-        }
-
-        tasarim.buttonEsittir.setOnClickListener {
-            val c = metin.substring(metin.length-1)
-            if((c != "+") && (metin.contains("+")))
-                metin = topla(metin)
-            tasarim.sonucEkrani.text = metin
-        }
-
-        tasarim.buttonTemizle.setOnClickListener {
-            metin = "0"
-            tasarim.sonucEkrani.text = metin
-        }
-
-        tasarim.buttonGerial.setOnClickListener {
-
-            tasarim.sonucEkrani.text = metin
-            if (tasarim.sonucEkrani.text.isEmpty())
-                tasarim.sonucEkrani.text = "0"
-            else
-                metin = metin.substring(0, metin.length-1)
-
-        }
-    }
-
-    private fun topla(islem: String): String {
-        val indeksArtı = islem.indexOf("+")
-        if(indeksArtı == islem.length)
-            return islem
-        val kisim1 = islem.subSequence(0, indeksArtı).toString()
-        val kisim2 = islem.subSequence(indeksArtı+1, islem.length).toString()
-        val sonuc = kisim1.toInt() + kisim2.toInt()
-
-        return sonuc.toString()
+        return buttons
     }
 }
